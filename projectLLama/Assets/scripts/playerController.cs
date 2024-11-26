fdb4711ee81cd7d8fa968f1e14a57d1089944b45
Batuhan <batuhan.ygit5544@gmail.com>
2024-11-26 21:38:38 +0300

    Deneme 2

---
+++ projectLLama/Assets/scripts/playerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private float speed = 10f;
+    private float jumpforce = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
